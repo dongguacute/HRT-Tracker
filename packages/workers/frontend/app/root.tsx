df19cea61caa91271437b51a760f1efa01c2d4c5
dongguacute <gudupao2022@gmail.com>
2026-03-04 23:22:51 +0800

    feat: add title to layout component for improved SEO and user experience
    - Included a title tag in the layout component to enhance search engine optimization and provide a clearer context for users visiting the application.

--- packages/workers/frontend/app/root.tsx
+++ packages/workers/frontend/app/root.tsx
@@ -99,6 +99,7 @@ export function Layout({ children }: { children: React.ReactNode }) {
   return (
     <html lang={i18n.language} suppressHydrationWarning>
       <head>
+        <title>HRT-Tracker</title>
         <meta charSet="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="manifest" href="/manifest.webmanifest" />
